fa7678f5df7b1a6a6e9abf1f2d52d8ce21cb6535
fabrivol <fabrivol@hotmail.com>
2019-05-21 13:50:57 -0300

    project working

--- collegeApp/views.py
+++ collegeApp/views.py
@@ -6,11 +6,11 @@ from django.shortcuts import render, redirect
 def historialnotas(request):
     return render(request, 'comments_history.html', {})
 
-def historialnotas(request):
-    if request.method == 'POST':
-        print(request.POST)
+#def historialnotas(request):
+ #   if request.method == 'POST':
+  #      print(request.POST)
         #comment_form = CommentForm(request.POST)
 
         #if comment_form.is_valid():
          #   comment_form.save()
-        return redirect()
+        #return redirect()
