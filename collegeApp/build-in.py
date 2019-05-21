d06705e370bbdee52246a85eae698e8a634ee70f
fabrivol <fabrivol@hotmail.com>
2019-05-21 13:09:22 -0300

    adding mile changes

---
+++ collegeApp/build-in.py
@@ -0,0 +1,6 @@
+from django.core.validators import validate_email
+from django.db import models
+
+models.EmailField()
+
+EmailField = models.CharField(validators= [validate_email])
--- collegeApp/views.py
+++ collegeApp/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
-from .forms import CommentForm
+#from .forms import CommentForm
 
 # Create your views here.
 
 def historialnotas(request):
+    return render(request, 'comments_history.html', {})
+
+def historialnotas(request):
     if request.method == 'POST':
         print(request.POST)
-        comment_form = CommentForm(request.POST)
+        #comment_form = CommentForm(request.POST)
 
-        if comment_form.is_valid():
-            comment_form.save()
+        #if comment_form.is_valid():
+         #   comment_form.save()
         return redirect()
