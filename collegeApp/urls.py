7bf4fb1aa32dcb51bfff8647894e4c75100f0952
fabrivol <fabrivol@hotmail.com>
2019-05-21 22:44:37 -0300

    working on em

--- collegeApp/urls.py
+++ collegeApp/urls.py
@@ -2,88 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', home),
-    path('', views.comments_history, name='comment_list'),
-    path('asistencia/', views.see_assistance),
+    # path('', home),
+
+    path('', views.comments_history, name='comments_list'),
+
+    path('asistencia/', views.see_assistance, name='assistance_list'),
+    path('calificaciones/', views.academic_progress, name='marks'),
+
     path('perfil/', views.teacher),
-    #path('',views.create_comment, name = 'comment_creation')
+    # path('',views.create_comment, name = 'comment_creation')
 
 ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- collegeApp/views.py
+++ collegeApp/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from collegeApp.models import Comment
+from collegeApp.models import Comment, Grades
 
-# Create your views here.
 
+@login_required(login_url="cuentas/login/")
 def comments_history(request):
-    return render(request, 'comments_history.html', {})
-
-
-def see_assistance(request):
-    return render(request, 'see_assistance.html')
+    # Historial de notas
+    comments = Comment.objects.all()
+    return render(request, 'comments_history.html', {'comments': comments})
 
 
 def academic_progress(request):
-    return render(request, 'academic_progress.html')
+    # Progreso Academico
+    grades = Grades.objects.all()
+    return render(request, 'academic_progress.html', {'grades': grades})
+
+
+
+
+def see_assistance(request):
+    # Asistencia
+    return render(request, 'see_assistance.html')
 
 
 def assistance(request):
@@ -35,7 +42,7 @@ def preceptor(request):
 def teacher(request):
     return render(request, 'teacher_userprofile.html')
 
-@login_required(login_url="accounts/login/")
-def home(request):
-    comments = Comment.objects.all()
-    return render(request, 'comments_history.html', {'comments': comments})
+# @login_required(login_url="cuentas/login/")
+# def home(request):
+#   comments = Comment.objects.all()
+#  return render(request, 'comments_history.html', {'comments': comments})
