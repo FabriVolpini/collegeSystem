e5f177131d845d8ebcd817f4a72fa69f7b1fe752
Milena <milelucero_09@hotmail.com>
2019-06-03 15:33:24 -0300

    "Falta arreglar form de inicio de sesión"

--- collegeApp/forms.py
+++ collegeApp/forms.py
@@ -105,6 +105,7 @@ class CourseCreationForm(forms.ModelForm):
             'shift': forms.TextInput(attrs={'class': 'col'})
         }
 
+
 class SubjectCreationForm(forms.ModelForm):
     class Meta:
         model = Subject
